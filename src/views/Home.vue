3341abc09bff7745680dd09be0c1f752e5e0b1a1
AkumuJey <josephowino7@gmail.com>
2022-11-14 23:29:55 +0300

    First Router Commit

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore asperiores rem corporis mollitia enim blanditiis velit delectus, vel, aperiam illum qui eum eos incidunt doloribus, at reiciendis! Quae laudantium suscipit excepturi optio impedit ea. Totam, consequuntur incidunt blanditiis suscipit voluptatibus aliquam architecto excepturi, ullam aut doloribus itaque repellendus labore nisi eligendi fuga minima perferendis molestias dolorum delectus. Totam, earum. Eveniet impedit labore sit facilis, reiciendis, modi qui eos iusto deleniti accusantium consectetur totam ipsam, veniam minus. Exercitationem tenetur ipsam fuga itaque fugit impedit et asperiores? Hic debitis perferendis esse fuga impedit non velit modi, cupiditate cum vero amet delectus eius.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore asperiores rem corporis mollitia enim blanditiis velit delectus, vel, aperiam illum qui eum eos incidunt doloribus, at reiciendis! Quae laudantium suscipit excepturi optio impedit ea. Totam, consequuntur incidunt blanditiis suscipit voluptatibus aliquam architecto excepturi, ullam aut doloribus itaque repellendus labore nisi eligendi fuga minima perferendis molestias dolorum delectus. Totam, earum. Eveniet impedit labore sit facilis, reiciendis, modi qui eos iusto deleniti accusantium consectetur totam ipsam, veniam minus. Exercitationem tenetur ipsam fuga itaque fugit impedit et asperiores? Hic debitis perferendis esse fuga impedit non velit modi, cupiditate cum vero amet delectus eius.</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
